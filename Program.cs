9315b1c4bb9d99faa1e3c1f38252d5503272c747
22adamhussain <22adamhussain@S006-20A>
2025-01-07 11:44:29 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace intro_to_vstudio_Y9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine(" World!");
+        }
+    }
+}
+ 
