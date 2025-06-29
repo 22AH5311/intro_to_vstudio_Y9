718237035613c689e40e2459a126914c9a1af1a3
22AH5311 <22adamhussain@heckgrammar.co.uk>
2025-06-29 21:24:47 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,9 +3,28 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello");
-            Console.WriteLine(" World!");
+        { bool accessgranted = false;
+    while (accessgranted = true){
+        Console.WriteLine("Please enter username");
+        string username = Console.ReadLine();
+        Console.WriteLine("PLease enter the password that goes with that username");
+        string password = Console.ReadLine();
+        if (username == "Yusuf5"){
+            while(password == "33kk"){
+                Console.WriteLine("Access Granted");
+                accessgranted = true;
+            }
+            
+                Console.WriteLine("Access denied");
+            }
+            else if (username == "Mary80"){
+                while(password == "af5r"){
+                    Console.WriteLine("Access Granted");
+                    accessgranted = true;
+                }
+                Console.WriteLine("Access Denied");
+            }
+    }
         }
     }
 }
